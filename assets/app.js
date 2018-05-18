fa88d439d27c7f24427e806c0f2a646df61731a6
Dylan Rutter <dylanrutter@Dylans-MBP.attlocal.net>
2018-05-18 11:44:12 -0500

    start

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+< script src = "https://www.gstatic.com/firebasejs/5.0.3/firebase.js" > < /script> <
+  script >
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBeVtM3uiHHsrljxxPLhW6WcM2Mb6Mz4ak",
+    authDomain: "train-d3564.firebaseapp.com",
+    databaseURL: "https://train-d3564.firebaseio.com",
+    projectId: "train-d3564",
+    storageBucket: "",
+    messagingSenderId: "166738016174"
+  };
+firebase.initializeApp(config); <
+/script>

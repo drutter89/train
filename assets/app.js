4c95209a9db4087c218250357181a1cef121a435
Dylan Rutter <dylanrutter@Dylans-MBP.attlocal.net>
2018-05-18 21:03:38 -0500

    progress

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,69 @@
-< script src = "https://www.gstatic.com/firebasejs/5.0.3/firebase.js" > < /script> <
-  script >
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBeVtM3uiHHsrljxxPLhW6WcM2Mb6Mz4ak",
-    authDomain: "train-d3564.firebaseapp.com",
-    databaseURL: "https://train-d3564.firebaseio.com",
-    projectId: "train-d3564",
-    storageBucket: "",
-    messagingSenderId: "166738016174"
-  };
-firebase.initializeApp(config); <
-/script>
+var config = {
+  apiKey: "AIzaSyBeVtM3uiHHsrljxxPLhW6WcM2Mb6Mz4ak",
+  authDomain: "train-d3564.firebaseapp.com",
+  databaseURL: "https://train-d3564.firebaseio.com",
+  projectId: "train-d3564",
+  storageBucket: "",
+  messagingSenderId: "166738016174"
+};
+firebase.initializeApp(config);
+
+// Initial Values
+var name = "";
+var destination = "";
+var frequency = "";
+var nextArrival = "";
+var minAway = "";
+var databaseTrain = firebase.database();
+var moment = moment().format();
+
+$(function () {
+  console.log("ready!");
+
+  // Capture Button Click
+  $("#trainBtn").on("click", function (event) {
+    event.preventDefault();
+
+    // Grabbed values from text-boxes
+    name = $("#name").val().trim();
+    destination = $("#destination").val().trim();
+    frequency = $("#frequency").val().trim();
+    nextArrival = $("#nextArrival").val().trim();
+    console.log(name);
+    console.log(destination);
+    console.log(frequency);
+    console.log(nextArrival);
+
+    // Code for "Setting values in the database"
+    databaseTrain.ref().push({
+      name: name,
+      destination: destination,
+      frequency: frequency,
+      nextArrival: nextArrival,
+      minAway: minAway
+    });
+  });
+
+
+  // Firebase watcher + initial loader HINT: .on("value")
+
+  databaseTrain.ref().on("value", function (snapshot) {
+
+
+
+      // Change the HTML to reflect
+
+
+      // Handle the errors
+    },
+    function (errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+
+    });
+
+  function getTrains() {
+
+    var ref = database().ref("dinosaurs");
+
+  }
+});
